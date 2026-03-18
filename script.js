1f66abce02930d8be8964996ca7ab379cfb4510b
I1HN <chriscaputi00@gmail.com>
2026-03-18 15:24:18 -0300

    Corrigido as variaveis

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 alert("Vamos multiplicar dois números?");
 
-let numUm;
-let numDois;
-let resultado; // da multiplicação
+let numUmMultiplicacao;
+let numDoisMultiplicacao; 
+let resultadoMultiplicacao; //resultado da multiplicação
 
 // Para realizar a multiplicação vamos utilizar dois números
 
-numUm = Number(prompt("Insira o primeiro número que deseja multiplicar"));
-numDois = Number(prompt("Agora, insira o segundo numero para multiplicarmos"));
-resultado = numUm*numDois;
+numUmMultiplicacao = Number(prompt("Insira o primeiro número que deseja multiplicar"));
+numDoisMultiplicacao = Number(prompt("Agora, insira o segundo numero para multiplicarmos"));
+resultadoMultiplicacao = numUmMultiplicacao*numDoisMultiplicacao;
 
-alert(`Multiplicando esses dois números, obtemos o resultado ${resultado.toFixed(2)}`);
+alert(`Multiplicando esses dois números, obtemos o resultado ${resultadoMultiplicacao.toFixed(2)}`);
