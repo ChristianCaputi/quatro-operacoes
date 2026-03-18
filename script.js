e15a052af49d27bd775c4e49eeacab0000748671
I1HN <sozisobrennolotier99@gmail.com>
2026-03-18 14:48:58 -0300

    Foi adicionado a operação de subtração entre as variáveis numUm e numDois. E o alert para escrever o resultado

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let numUm
-let numDois
+let numUm;
+let numDois;
+let resultado;
+
+numUm = Number(Prompt("Por favor, insira o primeiro número da subtração"));
+numDois = Number(Prompt("Por fim, insira o segundo número da subtração"));
+result = numUm - numDois;
+
+    alert(`O resultado da subtração dos dois números desejados é: ${resultado.tofixed(2)}!`);
