a0a391dd91869b57ff53f786d39e17a6cb6d4171
I1HN <chriscaputi00@gmail.com>
2026-03-18 14:22:43 -0300

    Adicionado o que as variaveis são

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let numUm 
-let numDois
-let resultado
+let numUm;
+let numDois;
+let resultado; // da multiplicação
 // Para realizar a multiplicação vamos utilizar dois números
+numUm = Number(prompt("Insira o primeiro número que deseja multiplicar"));
+numDois = Number(prompt("Agora, insira o segundo numero para multiplicarmos"));
+resultado = numUm*numDois;
 
