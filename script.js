6627ec85857862c924a3eeead5e12d09db1c1840
I1HN <goismunhoz2030@gmail.com>
2026-03-18 14:45:28 -0300

    comandos inseridos(teste 1)

---
+++ script.js
@@ -0,0 +1,10 @@
+let numUm;
+let numDois;
+let result;
+
+numUm = Number(prompt("Digite o primeiro número:"));
+numDois = Number(prompt("Digite o segundo número:"));
+
+result = numUm + numDois;
+
+alert(`O soma dos dois números é ${result}`);
