870ea34e1726488f52d6755ad896703741b406ef
I1HN <sozisobrennolotier99@gmail.com>
2026-03-18 15:36:35 -0300

    Foram modificados os seguintes itens: 1 - Os comentários do código foram adiicionados. 2 - Os nomes das variáveis foram modificadas para evitar conflitos

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let numUm;
-let numDois;
-let resultado;
+let numUmSubtracao; //A variável não foi nomeada apenas como "numUm" para que não ocorra conflitos ao ultilizar o "Merge"
+let numDoisSubtracao;
+let resultadoSubtracao; // Resultado da subtração
 
-numUm = Number(Prompt("Por favor, insira o primeiro número da subtração"));
-numDois = Number(Prompt("Por fim, insira o segundo número da subtração"));
-result = numUm - numDois;
+//As variáveis "nuUmSubtracao" e "numDoisSubtracao" foram definidas na linguagem Camel Case e representam os dois números da subtração. 
 
-    alert(`O resultado da subtração dos dois números desejados é: ${resultado.tofixed(2)}!`);
+numUmSubtracao = Number(Prompt("Por favor, insira o primeiro número da subtração"));
+numDoisSubtracao = Number(Prompt("Por fim, insira o segundo número da subtração"));
+resultadoSubtracao = numUm - numDois;
+
+//"tofixed" é uma ferramenta ultilizada para delimitar a quantidade de casas decimais do resultado. O número entre parênteses é essa quantidade de casas decimais.
+
+    alert(`O resultado da subtração dos dois números desejados é: ${resultadoSubtracao.tofixed(2)}!`);
