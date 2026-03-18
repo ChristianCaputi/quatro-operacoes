d4a31c7b9869fb6f5012d98c095f56677d2e4208
I1HN <goismunhoz2030@gmail.com>
2026-03-18 14:57:24 -0300

    inserção de comentários e do toFixed)

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+// let serve para criar uma variável
+
 let numUm;
 let numDois;
 let result;
 
+//  Number faz o carácter ser lido como número
+//  prompt faz aparecer uma caixa de texto para o usuário inserir os números desejados
+
 numUm = Number(prompt("Digite o primeiro número:"));
 numDois = Number(prompt("Digite o segundo número:"));
 
+//result é a soma das variáveis numUm + numDois
+
 result = numUm + numDois;
 
-alert(`O soma dos dois números é ${result}`);
+// alert serve para exibir uma menagem na tela do usuário
+
+alert(`O soma dos dois números é ${result.toFixed(2)}`);
