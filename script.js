713334a9ddfb8fa1e426a44355f5a8b90e642d97
I1HN <chriscaputi00@gmail.com>
2026-03-18 14:45:04 -0300

    Adicionado os alerts

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+alert("Vamos multiplicar dois números?");
+
 let numUm;
 let numDois;
 let resultado; // da multiplicação
+
 // Para realizar a multiplicação vamos utilizar dois números
+
 numUm = Number(prompt("Insira o primeiro número que deseja multiplicar"));
 numDois = Number(prompt("Agora, insira o segundo numero para multiplicarmos"));
 resultado = numUm*numDois;
 
+alert(`Multiplicando esses dois números, obtemos o resultado ${resultado.toFixed(2)}`);
